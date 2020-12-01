d31b30069a8efcec804cbe34bd02ce9e93dbcf16
Emily <linemily97@gmail.com>
2020-11-30 21:41:33 -0800

    exract enzyme setup

--- Desktop/jotto-react-testing-practice2/src/setupTests.js
+++ Desktop/jotto-react-testing-practice2/src/setupTests.js
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
+
+//create react app looks for this file first, will run this before every test
+
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
