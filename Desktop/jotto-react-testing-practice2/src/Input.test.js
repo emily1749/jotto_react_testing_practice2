0bcec0facbe0f4450cd80dfd82fd592da6e7f77c
Emily <linemily97@gmail.com>
2020-12-02 00:06:52 -0800

    input component

--- Desktop/jotto-react-testing-practice2/src/Input.test.js
+++ Desktop/jotto-react-testing-practice2/src/Input.test.js
@@ -3,6 +3,20 @@ import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
+
+//what did we do?
+//created a storeFactory utility that creats a testing store
+//with app reducers
+//added the store from the store factory
+//as a prop to our connected component]
+//we used shallow to create a virtualDOM of the connect component
+//used .dive() to get the child of the connect component (input)
+//use actual store not mock store.
+//there's redux-mock-store that can tell u the store...
+//actual store is closer to the app.
+//we used connected comoponent and then dive to get it inside.
+//we could also export input.....
+
 //we want to create a store for testing that
 // will match the configuration of our actual store
 
@@ -22,10 +36,24 @@ const setup = (initialState = {}) => {
 
 setup();
 describe('render', () => {
+  let wrapper;
+  beforeEach(() => {
+    const initialState = { success: false };
+    wrapper = setup(initialState);
+  });
   describe('word has not been guessed', () => {
-    test('renders component without error', () => {});
-    test('renders input box', () => {});
-    test('renders submit button', () => {});
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
+    });
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(1);
+    });
   });
 
   describe('word has been guessed', () => {
