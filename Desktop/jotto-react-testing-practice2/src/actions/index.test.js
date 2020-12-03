7ca7c1e8647babecc1726fb68a790ee842a64313
Emily <linemily97@gmail.com>
2020-12-02 17:46:06 -0800

    added integration testing, acitions, reducers

--- Desktop/jotto-react-testing-practice2/src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { correctGuess, actionTypes } from './';
-
-describe('correctGuess', () => {
-  test('returns an actions with type "CORRECT_GUESS"', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-    //toEqual is a deep equal
-  });
-});
---
+++ Desktop/jotto-react-testing-practice2/src/reducers/secretWordReducer.js
@@ -0,0 +1,5 @@
+export default (state = null, action) => {
+  return state;
+  //this will be called anytime called... do not
+  //want to null out.... return existing state
+};
