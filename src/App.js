02bf65f84f90090e88e4558eb00a3669ba33d5c0
Emily <linemily97@gmail.com>
2020-12-05 15:28:56 -0800

    add getsecretword action creator on app mount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
-export default App;
+export class UnconnectedApp extends Component {
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
+
+  return() {
+    this.return(
+      <div className='container'>
+        <h1> Jotto</h1>
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  const { success, gussedWords, secretWord } = state;
+  return { success, gussedWords, secretWord };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { storeFactory } from '../test/testUtils';
+import App, { UnconnectedApp } from './App';
+
+const setup = (state = {}) => {
+  const store = storeFactory(store);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+test("'getSecretWord' runs on App mount", () => {
+  const getSecretWordMock = jest.fn();
+  //this is a jest function that jest now
+  //watches to see when it's called and how.
+  //jest.fn()
+
+  //set up app component with getSeretWordMock as prop
+  //our real one gets it from redux
+  //but our one here will be prop passed in
+  //not setup function bc that uses app
+  //we're creating a shallow of unconnected
+
+  //getSecretWord = getSecretWordMock now will use mock funciton
+  //whenever getsecretword is called
+  //now will know when it calls getsecretword prop. bc feature of mock funciotn
+
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    gussedWords: [],
+  };
+  //set up app w getsecretmock as the getsecretword prop
+  const wrapper = shallow(<UnconnectedApp getSecretWord={...props} />);
+
+  //run lifecycle method
+  wrapper.instance().componentDidMount();
+
+  //check to see if mock ran
+  //mock gets mock properties
+  //calls is an array shows how many times called
+  //take the legnth of calls
+  const getSecretWordCallCOunt = getSecretWordMock.mock.calls.length;
+  expect(getSecretWordCallCount).toBe(1);
 });
